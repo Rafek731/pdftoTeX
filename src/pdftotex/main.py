219c5ae5a681c1d43c68e9cb477b6189b1679087
Rafał Mazur <raf.mazur.000@gmail.com>
2026-08-29 22:32:17 +0200

    feat: Add main program logic

--- src/pdftotex/main.py
+++ src/pdftotex/main.py
@@ -1,3 +1,31 @@
+import asyncio
+from pathlib import Path
+
+from google import genai
+
+from .AsyncTeXConverter import AsyncTeXConverter
+from .utils import cli, load_api_key
 
 
+async def run_program():
+    API_KEY = load_api_key()
+    client = genai.Client(api_key=API_KEY)
+    
+    args = cli.parse_arguments()
+    PROMTPS_DIR = Path(__file__).parent / "prompts"
+    prompt = (PROMTPS_DIR / "prompt.txt").read_text("utf-8")
+    prompt = prompt.replace("@LANGUAGE", args.language)
+    system_prompt = (PROMTPS_DIR / "system_prompt.txt").read_text("utf-8")
+    
+    converter = AsyncTeXConverter(
+        client, 
+        args.model,
+        prompt,
+        system_prompt,
+        args.max_parallel_files
+    )
 
+    await converter.convert_batch(args.input, args.output)
+
+def main():
+    asyncio.run(run_program())
